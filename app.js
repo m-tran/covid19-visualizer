1f6d783b1ee680b3b285a283f475e915fc639cb6
Michelle Tran <mptran0101@berkeley.edu>
2020-05-16 12:17:43 -0700

    render 3d globe

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.covid19api.com/summary",
+    }).then(function (res) {
+        console.log(res);
+    });
+
+    var $globeViz = document.querySelector("#globeViz");
+    
+    const world = Globe();
+
+    world($globeViz).globeImageUrl('https://unpkg.com/three-globe/example/img/earth-night.jpg');
+    world($globeViz).bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png');
+
 
 });
+
+// const myGlobe = Globe();
+
+// Globe({ configOptions })(<domElement>)
+
+// myGlobe(<myDOMElement>).globeImageUrl(<imageUrl>).pointsData(<myData>);
